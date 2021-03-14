7c5f8f77ccd6b7fa73f3adf84571075df0f08810
kilian <kiliantennyson@live.ie>
2021-03-14 12:22:09 +0000

    Added screen class

--- src/thequizmaster/Game.java
+++ src/thequizmaster/Game.java
@@ -1,7 +1,10 @@
 package thequizmaster;
 
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
 
 import javax.swing.JFrame;
 
@@ -41,8 +44,30 @@ public class Game extends Canvas implements Runnable{
 	
 	public void run() {
 		while(running) {
-			
+			update();
+			render();
 		}
+	}
+	
+	public void update() {
+
+	}
+	
+	public void render() {
+		BufferStrategy bs = getBufferStrategy();
+		if(bs == null) {
+			createBufferStrategy(3);
+			return;
+		}
+		
+		Graphics g = bs.getDrawGraphics();
+		//graphics
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, getWidth(), getHeight());
+		
+		//graphics end
+		g.dispose();
+		bs.show();
 	}
 
 	public static void main(String[] args) {
